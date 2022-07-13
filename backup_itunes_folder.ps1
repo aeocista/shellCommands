aed009c1475728ccf62700ec486f0dd04a9f5f1c
wesleyris <wesley.applequist@icloud.com>
2022-07-13 11:56:07 -0500

    Create backup_itunes_folder.ps1

---
+++ backup_itunes_folder.ps1
@@ -0,0 +1,4 @@
+# Copy from iTunes Music Directory to OneDrive iTunes Backup Directory
+robocopy  'C:\Users\wesle\Music\iTunes\iTunes Media\Music' 'C:\Users\wesle\OneDrive\Music\iTunes Backup' /MIR
+
+# Update Last Sync File
