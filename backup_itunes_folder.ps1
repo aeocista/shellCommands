2320c0de2927046676b4cb9cc848e5ea2d32c418
wesleyris <wesley.applequist@icloud.com>
2022-10-27 12:39:38 -0500

    Update backup_itunes_folder.ps1
    Added syncing for the entire directory to maintain album artwork and media metadata (date added, lyrics, et cetera). Also added logging for syncing and a command to "tail" the log in PowerShell.

--- backup_itunes_folder.ps1
+++ backup_itunes_folder.ps1
@@ -1,2 +1,5 @@
-# Copy from iTunes Music Directory to OneDrive iTunes Backup Directory
-robocopy  'C:\Users\wesle\Music\iTunes\iTunes Media\Music' 'C:\Users\wesle\OneDrive\Music\iTunes Backup' /MIR
+# Backup the iTunes Directory
+robocopy C:\Users\wesle\Music\iTunes C:\Users\wesle\OneDrive\Music\iTunes-Backup /MIR /R:1 > C:\Users\wesle\Music\iTunes-Backup.log
+
+# Follow the Backup Log
+Get-Content .\iTunes-Backup.log -Wait
